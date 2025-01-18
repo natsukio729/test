d2f650f429a72031393b0b1a3bd983d928c65fce
Natsuki Oshima <natuki.o.nakano@gmail.com>
2025-01-18 16:03:03 +0900

    Add large files tracked by Git LFS

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from .routes import main
 
 def create_app():
     app = Flask(__name__)
-    # Add your routes and configurations here
+    app.config['On202501'] = 'your_secret_key'
+    app.register_blueprint(main)
     return app
